9ddb29f5761af877eee0ceda577f0f72e8b2988d
Make <matetothuk@gmail.com>
2021-02-12 16:20:34 +0100

    re added

---
+++ src/app/components/start/startCompany.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {StartComponent} from './start.component';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.css']
+})
+export class StartCompanyComponent extends StartComponent implements OnInit {
+  greeting = 'Hello world!';
+
+  constructor() {
+    super();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
