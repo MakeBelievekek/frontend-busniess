36b6e86fd4fb852dfa1775ddab3cdfbcc6143e0f
matetothuk@gmail.com <matetothuk@gmail.com>
2021-02-12 21:01:08 +0000

    ez kell

--- src/app/components/start/startCompany.component.ts
+++ src/app/components/start/startCompany.component.ts
@@ -7,11 +7,6 @@ import {StartComponent} from './start.component';
   styleUrls: ['./start.component.css']
 })
 export class StartCompanyComponent extends StartComponent implements OnInit {
-<<<<<<< HEAD
-=======
-  superSecret: string;
-  moreSecret: string;
->>>>>>> 6edeb75... more secret
 
   constructor() {
     super();
