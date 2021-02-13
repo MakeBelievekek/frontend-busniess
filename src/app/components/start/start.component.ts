c9295339a09f2ba709ef644ad38b4032dc5c09fc
Make <matetothuk@gmail.com>
2021-02-13 00:04:59 +0000

    more changes

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -8,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class StartComponent implements OnInit {
   greeting = 'Hello world!';
   additionalData: string;
+  additionalDataTwo: string;
 
   constructor() {
   }
